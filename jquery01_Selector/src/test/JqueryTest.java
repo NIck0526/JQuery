4f77068d0e8091fc8577c93e57892da99ff72a0f
NIck0526 <lee26@naver.com>
2017-09-08 18:01:42 +0900

    09/08

---
+++ jquery01_Selector/src/test/JqueryTest.java
@@ -0,0 +1,14 @@
+package test;
+
+public class JqueryTest {
+	// 자바 기반의 클래스... 동적인 문서에 해당한다.
+	// 브라우저에서 실행되는 기반의 기술이 아니다.
+	// Back End 기술
+	// jvm 기반의 컨테이너에서 실행되어 진다.
+	
+	String name;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
